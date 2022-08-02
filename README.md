f74b49967c912240535d4dfdebc158b7bc8fecb0
parkforte <parkforte@naver.com>
2022-08-02 14:26:09 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,35 @@
+# flutter_app
+
+A new Flutter project.
+
+## Getting Started
+
+This project is a starting point for a Flutter application.
+
+A few resources to get you started if this is your first Flutter project:
+
+- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
+- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
+
+For help getting started with Flutter development, view the
+[online documentation](https://docs.flutter.dev/), which offers tutorials,
+samples, guidance on mobile development, and a full API reference.
+
+##프로젝트 구조의 이해
+- 프로젝트 구성 폴더
+    - .idea : 개발 도구에 필요한 설정
+    - android : 안드로이드 네이티브 코드를 작성하는 부분
+    - build : 빌드시 생성되는 파일
+    - ios : ios 네이티브 코드를 작성하는 부분
+    - lib : drat 코드를 작성하는 부분
+    - test : test 코드를 작성하는 부분
+
+- 프로젝트 구성 파일
+    - .gitignore
+    - .metadata : 프로젝트가 관리하는 파일 (임의수정x)
+    - .packages : 각종 패키지 정보 (임의수정x)
+    - flutter_app.iml : 개발 도구에 필요한 설정 파일 (임의수정x)
+    - pubspec.lock : 패키지매니저가 이용하는 파일 (임의수정x)
+    - pubspec.yaml : 패키지매니저가 이용하는 파일
+  
+
---
+++ lib/main.dart
@@ -0,0 +1,121 @@
+import 'package:flutter/material.dart';
+
+// 앱 시작 부분
+void main() {
+  runApp(const MyApp());
+}
+
+// 시작 클래스. 머티리얼 디자인 앱 생성
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: '플러터 데모 앱'),  //표시할 화면의 인스턴스
+    );
+  }
+}
+
+// 시작 클래스가 실제로 표시하는 클래스. 카운터 앱 화면
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+// 위 MyHomePage 클래스의 상태를 나타내는 State 클래스
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  // _counter 변수를 1 증가시키고 화면을 다시 그리는 메서드
+  void _incrementCounter() {
+    setState(() { // 화면을 다시 그리도록 하는 함수. StatefulWidget만 가능
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
+  }
+
+  // 화면에 UI를 그리는 메서드. 그려질 위젯을 반환
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(  // 머티리얼 디자인 기본 뼈대 위젯
+      appBar: AppBar( // 상단 앱바
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: Center( // 표시할 내용
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          // Column is also a layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Invoke "debug painting" (press "p" in the console, choose the
+          // "Toggle Debug Paint" action from the Flutter Inspector in Android
+          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+          // to see the wireframe for each widget.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',  // _counter 변수를 표시
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter, // 클릭시 _incrementCounter() 메서드를 실행
+        tooltip: 'Increment',
+        child: const Icon(Icons.add), //상단앱바
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
